b4ca8b42a5545358bd61d3558e7f53a77d7f3f02
Oberst Tanja <taob@gmx.de>
2014-10-22 21:34:26 +0200

    Registration View Controller.
    App crashes

---
+++ Blunch/BlunchRegistrationViewController.m
@@ -0,0 +1,37 @@
+//
+//  BlunchRegistrationViewController.m
+//  Blunch
+//
+//  Created by Oberst Tanja on 22.10.14.
+//  Copyright (c) 2014 Oberst Tanja. All rights reserved.
+//
+
+#import "BlunchRegistrationViewController.h"
+
+@interface BlunchRegistrationViewController ()
+
+@end
+
+@implementation BlunchRegistrationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
