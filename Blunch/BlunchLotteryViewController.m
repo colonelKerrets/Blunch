d5a66a17e06cacddf36caedcb919a4d13bacfb33
Oberst Tanja <taob@gmx.de>
2014-10-23 00:53:18 +0200

    Set NSDictionary *myData for name(key) and email(value)
    For randomly selected row, set emailadress:
    
        NSInteger row = [self.lotteryPicker selectedRowInComponent:0];
        NSString *key = [self.myData.allKeys objectAtIndex:row];
        NSString *mail = [self.myData valueForKey:key];
    
        NSArray *toRecipents = [NSArray arrayWithObject:mail];

--- Blunch/BlunchLotteryViewController.m
+++ Blunch/BlunchLotteryViewController.m
@@ -12,10 +12,14 @@
 @interface BlunchLotteryViewController ()
 
 @property (strong, nonatomic) NSArray *blunchNames;
+@property (strong, nonatomic) NSDictionary  *myData;
+
 @property (strong, nonatomic) NSArray *emailTitle;
 @property (strong, nonatomic) NSArray *messageBody;
 @property (strong, nonatomic) NSArray *toRecipents;
 @property (weak, nonatomic) IBOutlet UIPickerView *lotteryPicker;
+
+@property (nonatomic) BOOL alreadyChoosen;
 
 @end
 
@@ -28,7 +32,12 @@
     // Email Content
     NSString *messageBody = @"<h1>Learning iOS Programming!</h1>"; // Change the message body to HTML
     // To address
-    NSArray *toRecipents = [NSArray arrayWithObject:@"support@appcoda.com"];
+    
+    NSInteger row = [self.lotteryPicker selectedRowInComponent:0];
+    NSString *key = [self.myData.allKeys objectAtIndex:row];
+    NSString *mail = [self.myData valueForKey:key];
+    
+    NSArray *toRecipents = [NSArray arrayWithObject:mail];
     
     MFMailComposeViewController *mc = [[MFMailComposeViewController alloc] init];
     mc.mailComposeDelegate = self;
@@ -38,9 +47,7 @@
     
     // Present mail view controller on screen
     [self presentViewController:mc animated:YES completion:NULL];
-    
 
-  
 }
 
 
@@ -54,7 +61,7 @@
         case MFMailComposeResultCancelled:
             [[[UIAlertView alloc] initWithTitle:@"Oooops"
                                         message:@"Are you sure? ;)"
-                                       delegate:nil
+                                       delegate:self
                               cancelButtonTitle:@"OK"
                               otherButtonTitles:nil] show];
             break;
@@ -64,7 +71,7 @@
         case MFMailComposeResultSent:
             [[[UIAlertView alloc] initWithTitle:@"Awesome"
                                         message:@"Thanks for sending the mail."
-                                       delegate:nil
+                                       delegate:self
                               cancelButtonTitle:@"OK"
                               otherButtonTitles:nil] show];
             break;
@@ -79,7 +86,11 @@
     [self dismissViewControllerAnimated:YES completion:NULL];
 }
 
-
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    [self performSegueWithIdentifier: @"historySegue" sender: self];
+    
+  
+}
 
 #pragma mark - UIPickerViewDataSource
 
@@ -87,55 +98,70 @@
     return 1;
 }
 - (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
-    return [self.blunchNames count];
+    return self.myData.count;
+    
+//    return [self.blunchNames count];
 }
 
 #pragma mark - UIPickerViewDelegate
 
 - (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
-    return self.blunchNames [row];
+    return [self.myData.allKeys objectAtIndex:row];
+    
+//    return self.blunchNames [row];
 }
 
 #pragma mark - View
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    [self.lotteryPicker selectRow: (arc4random() % [self.blunchNames
-                                                count]) inComponent: 0 animated: YES];
+    
+    if (!self.alreadyChoosen) {
+        [self.lotteryPicker selectRow: (arc4random() % [self.myData.allKeys
+                                                        count]) inComponent: 0 animated: YES];
+          self.alreadyChoosen = YES;
+    }
+
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    [self.lotteryPicker selectRow:2 inComponent:0 animated:YES];
+    
+//    if (!self.alreadyChoosen) {
+//        [self.lotteryPicker selectRow:2 inComponent:0 animated:YES];
+//        self.alreadyChoosen = YES;
+//    }
+    
 }
 
 - (void)viewDidLoad {
+
     [super viewDidLoad];
     self.navigationItem.hidesBackButton = YES;
+
+    self.alreadyChoosen = NO;
+    
+    
+    self.myData = @{@"Tanja": @"tanja.oberst@foo.gmx",
+                    @"Ida": @"Ida@foo.gmx",
+                    @"Sandra": @"sandra@foo.gmx",
+                    @"Jonas": @"Jonas@foo.gmx"};
+    
+
+    
     
     self.blunchNames = @[@"Tanja", @"Ida", @"Sandra", @"Jonas", @"Paul", @"Petter"];
+    
+    
+    
     self.blunchNames = [self.blunchNames arrayByAddingObjectsFromArray: self.blunchNames];
     
 }
-
-
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
